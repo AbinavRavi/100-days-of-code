b7dae6a3264a3aa3c3f13d49cba0e0a9e341a590
AbinavRavi <abinav.ravi@tum.de>
2022-01-21 22:11:13 +0100

    day1 complete

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/abinav/programming/100-days-of-code/day1/target/debug/day1: /home/abinav/programming/100-days-of-code/day1/src/main.rs
--- day1/target/debug/deps/day1-f6bf04934a365449.d
+++
@@ -1,5 +0,0 @@
-/home/abinav/programming/100-days-of-code/day1/target/debug/deps/day1-f6bf04934a365449: src/main.rs
-
-/home/abinav/programming/100-days-of-code/day1/target/debug/deps/day1-f6bf04934a365449.d: src/main.rs
-
-src/main.rs:

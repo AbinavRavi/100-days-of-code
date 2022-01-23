11d597849e2e456da58c06fe43d2f5e149c0c791
AbinavRavi <abinav.ravi@tum.de>
2022-01-24 00:11:21 +0100

    day3 complete

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -5,21 +5,36 @@ struct Calculator{
 
 impl Calculator{
     fn add(&self) -> u32{
-        self.num1 + self.num2;
+        let add = self.num1 + self.num2;
+        return add;
     }
 
-    fn subtract(&self) -> u8{
+    fn subtract(&self) -> u32{
+        let sub;
         if self.num1 > self.num2{
-            self.num1 - self.num2;
+            sub = self.num1 - self.num2;
         }
         else{
-            self.num2 -self.num1;
+            sub = self.num2 -self.num1;
         }
+        return sub;
+    }
+
+    fn multiply(&self) -> u32 {
+        let mult;
+        mult = self.num1 *self.num2;
+        return  mult;
+
     }
 
 
 }
 
 fn main() {
-    println!("Hello, world!");
+    let nums = Calculator{
+        num1: 10,
+        num2: 20,
+    };
+
+    print!("The calculator values are {},{},{}",nums.add(),nums.subtract(),nums.multiply());
 }

4a7e49015b7e153f5dc8e160f4eb499a1008d8d3
AbinavRavi <abinav.ravi@tum.de>
2022-01-28 22:23:53 +0100

    day8 in progress

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,3 +1,27 @@
+enum States{
+    TamilNadu,
+    AndhraPradesh,
+    Karnataka,
+    Telangana,
+    Kerala,
+}
+
+fn match_language(state:States) -> &'static str{
+    match state{
+        States::TamilNadu => "Tamil",
+        States::AndhraPradesh => "Telugu",
+        States::Kerala => "Malayalam",
+        States::Telangana => "Telugu",
+        States::Karnataka => "Kannada",
+        _ => "Some Other language",
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state:States = States::TamilNadu;
+
+    let language = match_language(state);
+
+    // println!("IN {:?} the official language is {:?}",state,language);
+
 }
